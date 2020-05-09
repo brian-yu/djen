1852d2d2153ce6aece13c366f72a753d499e3763
Brian Yu <brian@byu.io>
2020-05-09 00:33:48 -0400

    Update sample.

--- frontend/src/editor/sample.js
+++ frontend/src/editor/sample.js
@@ -4,5 +4,6 @@ export const sample = `// Put your code here and hit run to see it on the right!
 // You can also use p5.js and css-doodle.
 // Check out the gallery if you need some inspiration.
 
-// Note: try to make your art responsive to different aspect ratios.
+// Note: try to make your art responsive to different aspect ratios and
+// screen sizes.
 `;
