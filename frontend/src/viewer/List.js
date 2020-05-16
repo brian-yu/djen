9b88c9ee66e61511bdfcfbed688c5e51819f85bc
Brian Yu <brian@byu.io>
2020-05-16 01:36:53 -0400

    Fix list spacing.

--- frontend/src/viewer/List.js
+++ frontend/src/viewer/List.js
@@ -8,17 +8,25 @@ function List(props) {
   const [auth, setAuth] = useContext(AuthContext);
   const list = props.list;
 
-  return list.map((submission, idx) => {
-    return (
-      <Item key={submission.id}>
-        <Submission submission={submission} idx={idx} />
-      </Item>
-    );
-  });
+  return (
+    <ul>
+      {list.map((submission, idx) => {
+        return (
+          <Item key={submission.id}>
+            <Submission submission={submission} idx={idx} />
+          </Item>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default List;
 
-const Item = styled.div`
+const Item = styled.li`
   margin: 50px 0px;
+
+  &:first-child {
+    margin: 20px 0px;
+  }
 `;
